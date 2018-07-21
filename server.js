7b465078fa515585458ecb6084d8807e095d71ce
omes000 <omes000@gmail.com>
2018-07-21 10:12:07 -0500

    updating some server files

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/itemize",
+  process.env.MONGODB_URI || "mongodb://localhost/projectlonghorn",
   {
     useMongoClient: true
   }
